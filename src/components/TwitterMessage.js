8c7364a574281d2cd0d3f1daf7cdbe29581cb66b
Cloudymolecule <baires82@gmail.com>
2021-04-25 11:55:11 -0400

    commit tree f59c5e03b3ded6047421addd990aa2542f14596c

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,29 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      message: event.target.value
+    })
+  }
+
+  handleCounter = () => {
+    let counter = this.props.maxChars - this.state.message.length
+    return counter
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.state.message}/>
+        <br></br>
+        <h1>{this.handleCounter()}</h1>
       </div>
     );
   }
